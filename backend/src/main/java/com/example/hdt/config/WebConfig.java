77ed54bc9ab70e0c6503064f5ee9387284995ab3
Joey <s223091@dtu.dk>
2024-04-04 11:57:21 +0200

    Fetching patientList from DB

---
+++ backend/src/main/java/com/example/hdt/config/WebConfig.java
@@ -0,0 +1,19 @@
+/*package com.example.hdt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Adjust the port accordingly if your frontend runs on a different port
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+*/
