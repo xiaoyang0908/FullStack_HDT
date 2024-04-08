ac791e71798344693966a75b0653bbc3fa4ff47c
s220056 <s220056@student.dtu.dk>
2024-04-08 15:35:23 +0200

    fix database to set the status

--- backend/src/main/java/com/example/hdt/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hdt.repositories;
-
-import com.example.hdt.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-
-}
