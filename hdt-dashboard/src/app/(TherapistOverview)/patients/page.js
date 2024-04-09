8547f634e124da873feecf2a648aac99b87c0651
Joey <s223091@dtu.dk>
2024-04-09 11:15:36 +0200

    heeeeeelp

--- hdt-dashboard/src/app/(TherapistOverview)/patients/page.js
+++ hdt-dashboard/src/app/(TherapistOverview)/patients/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from "react";
 
-export default function Patients(){
+export default function TherapistPatientsDetails(){
 
     useEffect(() => {   // Prevents scrolling on page
         document.body.style.overflow = 'hidden';
