46b6d77b85bedfaa2650ad4af3fe7a5d1267840d
s220056 <s220056@student.dtu.dk>
2024-05-22 12:11:03 +0200

    update ui

--- hdt-dashboard/next.config.mjs
+++ hdt-dashboard/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        // Spring Boot API URL
+        API_URL: 'https://172.205.8.19/'
+      }
 };
 
 export default nextConfig;
