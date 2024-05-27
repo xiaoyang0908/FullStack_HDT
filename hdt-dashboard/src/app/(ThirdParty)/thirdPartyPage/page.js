defff810472f7d48876aa49f2ea67de52e33196b
s220056 <s220056@student.dtu.dk>
2024-05-28 00:33:14 +0200

    fix ui in 3rd

--- hdt-dashboard/src/app/(ThirdParty)/thirdPartyPage/page.js
+++ hdt-dashboard/src/app/(ThirdParty)/thirdPartyPage/page.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import ThreeDAvatar from "@/app/components/three";
 import Tooltip from '@mui/material/Tooltip';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbUpIcon from '@mui/icons-material/ThumbUpOutlined';
 import IconButton from '@mui/material/IconButton';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import {
@@ -131,40 +131,56 @@ export default function TrdPage() {
 
 
     const LiveStreamCard = () => (
-        <Grid sx={{ p: 3, height: "70%", overflow: 'hidden', backgroundImage:"/caregiverWelcome.svg"}}>
-            <Box sx={{flexGrow:1}} mb={5}>
-                <div><Typography variant="h6" align="left">Welcome!</Typography></div>
-                <div><Typography variant="h7" align="left">Cheer on {carePatient.name} right here</Typography></div>
+        <Grid sx={{ p: 5, height: "100%", overflow: 'hidden', 
+        backgroundImage:"url(3rdParty/welcomeBg.svg)", 
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', 
+        display:"flex",
+        justifyContent:"space-between"
+        }}>
+            <Box sx={{display:"flex", flexDirection:"column"}}>
+                <Box sx={{flexGrow:1, justifyContent:"center", flexDirection:"column", display:"flex"}} mb={5}>
+                    <div><Typography variant="h5" align="left" fontWeight={"bold"} color="white">Welcome!</Typography></div>
+                    <div><Typography variant="h6" align="left" fontWeight={"bold"} color="white">Cheer on {carePatient.name} right here</Typography></div>
+                </Box>
+                <Box>
+                    <Tooltip title={watchLiveEnabled ? "" : "Can only be used while in the same room"} placement="top">
+                        <span>
+                            <Button disabled={!watchLiveEnabled} sx={{borderRadius:"12px", fontWeight:"bold", bgcolor:"#EEF0F9", color:"#969696", width:"150px", height:"40px"}}>
+                                Exercise Live
+                            </Button>
+                        </span>
+                    </Tooltip>
+                    <Tooltip title={`This will show the live feed of what ${carePatient.name} is seeing and doing`} sx={{ ml: 1 }}>                
+                    <IconButton aria-label="help" color="white">
+                        <HelpOutlineIcon fontSize="small" />
+                    </IconButton>
+                    </Tooltip>
+                </Box>
             </Box>
-            <Tooltip title={watchLiveEnabled ? "" : "Can only be used while in the same room"} placement="top">
-                <span>
-                    <Button variant="outlined" disabled={!watchLiveEnabled}>
-                        Watch Live
-                    </Button>
-                </span>
-            </Tooltip>
-            <Tooltip title={`This will show the live feed of what ${carePatient.name} is seeing and doing`} sx={{ ml: 1 }}>                
-            <IconButton aria-label="help">
-                <HelpOutlineIcon fontSize="small" />
-            </IconButton>
-            </Tooltip>
+            <Box height="130%">
+                <img src="3rdParty/welcomeIcon.svg" width="100%" height="100%"/>
+            </Box>
         </Grid>
     );
 
     const TherapistCard = () => (
-        <Grid container alignItems="center" sx={{ p: 3, height: "100%", overflow: 'hidden' }}>
+        <Grid container alignItems="center" sx={{ p: 5, height: "100%", overflow: 'hidden' }}>
             <Avatar alt={'currentPatient.name'} src={"/profilePictureTherapist.png"} sx={{ width: 100, height: 100 }} />
             <Box sx={{ pl: 3 }}>
-                <div><Typography variant="h6" fontWeight="bold" color="#0D2560"></Typography>{carePatient.contact && carePatient.contact.fullName ? carePatient.contact.fullName : ""}</div>
-                <div><Typography variant="h7" align="left">Contact person for {carePatient.name}</Typography></div>
+                <div><Typography variant="h6" fontWeight="bold" color="#0D2560">{carePatient.contact && carePatient.contact.fullName ? carePatient.contact.fullName : ""}</Typography></div>
+                <div>
+                    <Typography variant="h7" align="left" color="#0D2560">Contact person for </Typography>
+                    <Typography variant="h7" align="left" color="#0D2560" fontWeight={"bold"}>{carePatient.name}</Typography>
+                </div>
             </Box>
             <Box mt={1} sx={{ width: '100%' }}>
                 <Grid container direction="row" justifyContent="space-between">
-                    <Typography variant="h7" align="left">Therapist:</Typography>
-                    <Typography variant="h7" align="right" fontWeight="bold" color="#0D2560"> {careTherapist.name}</Typography>
+                    <Typography variant="h7" align="left" color="#0D2560">Therapist:</Typography>
+                    <Typography variant="h7" align="right" fontWeight="bold" color="#0D2560"> Dr. {careTherapist.name}</Typography>
                 </Grid>
                 <Grid container direction="row" justifyContent="space-between">
-                    <Typography variant="h7" align="left">Contact number:</Typography>
+                    <Typography variant="h7" align="left" color="#0D2560">Contact number:</Typography>
                     <Typography variant="h7" align="right" fontWeight="bold" color="#0D2560">{careTherapist.phone}</Typography>
                 </Grid>
             </Box>
@@ -172,29 +188,40 @@ export default function TrdPage() {
     );
 
     const ThumbsUpCard = () => (
-        <Grid sx={{ p: 3, height: "70%", overflow: 'hidden', display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
-            <ThumbUpIcon fontSize="large"  sx={{ mb: 4, mt: 1, flexGrow:1 }} />
-            <Typography variant="h7">Thumbs up given</Typography>
-            <Typography variant="h4">{thumbsCount}</Typography>
+        <Grid sx={{ p: 5, height: "100%", overflow: 'hidden', alignItems: 'left', 
+        backgroundImage:"url(3rdParty/thumbsBg.svg)",
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', 
+        display:"flex",
+        justifyContent:"space-between"
+       }}>
+            <Box sx={{height:"100%",display:"flex",flexDirection:"column",alignItems:"left",justifyContent:"space-between"}}>
+                <img src="3rdParty/thumbIcon.svg" width="60%" />
+                <Typography variant="h6">Thumbs-up Count </Typography>
+                <Typography variant="h4" fontWeight={"bold"}>{thumbsCount}</Typography>
+            </Box>
+            <Box height={"100%"}>
+                <img src="3rdParty/thumbsBg2.svg" width="100%" height="100%"/>
+            </Box>
         </Grid>
     );
 
     const UserCard = () => (
-        <Grid container alignItems="center" sx={{ p: 3, height: "100%", overflow: 'hidden' }}>
+        <Grid container alignItems="center"  sx={{ p: 5, height: "100%", overflow: 'hidden'}}>
             <Avatar alt={'currentPatient.name'} src={carePatient.photo} sx={{ width: 100, height: 100 }} /* This is profile picture avatar */ /> 
             <Box sx={{ pl: 3 }}>
-                <div><Typography variant="h6">{carePatient.name}</Typography></div>
+                <div><Typography variant="h5" fontWeight="bold">{carePatient.name}</Typography></div>
                 <div>
-                    <Typography variant="h4" align="left" display="inline">79</Typography>
-                    <Typography variant="h7" align="left" display="inline"> hours exercised</Typography>
+                    <Typography variant="h3" align="left" display="inline">{carePatient.totalExerciseHours} </Typography>
+                    <Typography variant="h6" align="left" display="inline"> hours exercised</Typography>
                 </div>
             </Box>
             <Box mt={1} sx={{ width: '100%' }}>
                 <Grid container my={1} direction="row">
-                    <Typography variant="h7" align="left">Click the button to cheer on {carePatient.name}</Typography>
+                    <Typography variant="h6" align="left">Click the button to cheer on {carePatient.name}</Typography>
                 </Grid>
             </Box>
-            <Button variant="outlined" startIcon={<ThumbUpIcon />} onClick={()=>handleClickThumbsUp()} size="large">
+            <Button startIcon={<ThumbUpIcon />} onClick={()=>handleClickThumbsUp()} size="large" sx={{ color:"white", bgcolor:"#D68E56" , fontWeight:"bold", width:"220px", height:"50px" , borderRadius:"15px"}}>
                 Give thumbs up
             </Button>
             <Tooltip title="Gives a thumps up to motivate"
@@ -242,9 +269,9 @@ export default function TrdPage() {
             flexDirection: 'row',
         }}>
             <Grid container spacing={3}>
-                <Grid item xs={12} md={8}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={6}>
+                <Grid item xs={12} md={8} >
+                    <Grid container spacing={3} >
+                        <Grid item xs={6} >
                             <Paper sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
                                 <LiveStreamCard />
                                 <Divider />
@@ -266,19 +293,21 @@ export default function TrdPage() {
                     </Grid>
                 </Grid>
 
-                <Grid item xs={12} md={4} sx={{position:"relative"}}>
-                    <Box sx={{ width: '100%', height: '100%' }}>
-                        <ThreeDAvatar glbModelUrl={carePatient.avatar ? carePatient.avatar : ""} />
-                    </Box>
-                    <Box sx={{top:"4%", position:"absolute", left:"8%",width:"100%", height:"100%"}}>
-                        <Typography variant="h4" color={"white"}>Range of Motion(ROM)</Typography>
-                    </Box>
-                    <Box sx={{position:"absolute",width:"100%", height:"100%", top:0, left:"2%"}}>
-                        <img alt="rangeMotion"  src="ROMArrows.png" width={"100%"} height={"100%"} />
-                    </Box>
-                    <Box sx={{top:"55%", position:"absolute", left:"2%",width:"100%", height:"100%", display:"flex", justifyContent:"space-between", p:"10%" }}>
-                        <Typography variant="h5" color={"white"}>Left: {carePatient.performace[0].left}°</Typography>
-                        <Typography variant="h5" color={"white"}>right: {carePatient.performace[0].right}°</Typography>
+                <Grid item xs={12} md={4}>
+                    <Box  sx={{position:"relative", width: '100%', height: '100%' }}>
+                        <Box sx={{ width: '100%', height: '100%' }}>
+                            <ThreeDAvatar glbModelUrl={carePatient.avatar ? carePatient.avatar : ""} />
+                        </Box>
+                        <Box sx={{top:"4%", position:"absolute", left:"8%",width:"100%", height:"100%"}}>
+                            <Typography variant="h4" color={"white"}>Range of Motion(ROM)</Typography>
+                        </Box>
+                        <Box sx={{position:"absolute",width:"100%", height:"100%", top:0, left:"2%"}}>
+                            <img alt="rangeMotion"  src="ROMArrows.png" width={"100%"} height={"100%"} />
+                        </Box>
+                        <Box sx={{top:"55%", position:"absolute", left:"2%",width:"100%", height:"100%", display:"flex", justifyContent:"space-between", p:"10%" }}>
+                            <Typography variant="h5" color={"white"}>Left: {carePatient && carePatient.performance && carePatient.performance.length > 0 ? carePatient.performance[0].left : 0}°</Typography>
+                            <Typography variant="h5" color={"white"}>right: {carePatient && carePatient.performance && carePatient.performance.length > 0 ? carePatient.performance[0].right : 0}°</Typography>
+                        </Box>
                     </Box>
                 </Grid>
             </Grid>
